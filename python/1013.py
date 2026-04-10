0daf8348b0a4ecaa409443f5e74284b4b95b27ab
ManamiMayoki <manamimarufa@gmail.com>
2026-04-10 22:47:42 +0600

    1013

--- python/1013.py
+++ python/1013.py
@@ -1,13 +1,9 @@
-# Read three integers
 a, b, c = map(int, input().split())
 
-# Function to find greatest of two numbers
 def maior(x, y):
     return (x + y + abs(x - y)) // 2
 
-# Apply formula
 maior_ab = maior(a, b)
 maior_abc = maior(maior_ab, c)
 
-# Output result
 print(f"{maior_abc} eh o maior")
