980c0727b5a1ba5553fd0a06a339b67f5a277329
ManamiMayoki <manamimarufa@gmail.com>
2026-04-21 23:18:00 +0600

    fibo

--- Basic_Py/1.py
+++ Basic_Py/1.py
@@ -1,5 +1,13 @@
-print("Hello World!")
 
-print('She said "great job" and gave me a high five!')
-print("She said \"great job\" and gave me a high five!")
-print("Titanic competition")
+def fib(n):
+    a, b = 0, 1
+    i = 0
+    c=-2
+    while i < (c+n):
+        print(a,b)
+        
+        a, b = b, a + b
+        
+        i += 1
+    print(a)
+fib(4)
