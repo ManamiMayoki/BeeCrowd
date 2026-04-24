378e538740539fc9526d5c1c32027c79f15f3e93
ManamiMayoki <manamimarufa@gmail.com>
2026-04-24 10:23:53 +0600

    longest common prefix

---
+++ leetcode/Basic_longest_prefix.py
@@ -0,0 +1,14 @@
+def longestCommonPrefix(strs):
+    if not strs: #input list is empty
+        return ""
+    
+    for i in range(len(strs[0])):
+        char=strs[0][i]
+        for j in range(1,len(strs)):
+            if i==len(strs[j]) or strs[j][i]!=char: #corrent string shorter than reference, :->up to
+                return strs[0][:i]
+    return strs[0]
+
+words=input().split()
+print(longestCommonPrefix(words))
+    
